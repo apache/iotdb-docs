25ec07289f2da057a88dd31b359b45932845320d
CritasWang <critas@outlook.com>
2023-12-08 18:14:01 +0800

    revert(remove v1.3):

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -24,7 +24,7 @@ export const enNavbar = navbar([
     text: 'Documentation',
     children: [
       // { text: 'latest', link: '/UserGuide/Master/QuickStart/QuickStart' },
-      { text: 'v1.3.x', link: '/UserGuide/V1.3.x/QuickStart/QuickStart' },
+      // { text: 'v1.3.x', link: '/UserGuide/V1.3.x/QuickStart/QuickStart' },
       { text: 'v1.2.x', link: '/UserGuide/V1.2.x/QuickStart/QuickStart' },
       { text: 'v1.1.x', link: '/UserGuide/V1.1.x/QuickStart/QuickStart' },
       { text: 'v1.0.x', link: '/UserGuide/V1.0.x/QuickStart/QuickStart' },
