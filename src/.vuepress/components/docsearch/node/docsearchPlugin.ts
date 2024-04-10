a8974ccd9d6e20410f86c498c3d7292b458b3773
CritasWang <critas@outlook.com>
2024-04-10 17:44:10 +0800

    update vuepress (#216)

--- src/.vuepress/components/docsearch/node/docsearchPlugin.ts
+++ src/.vuepress/components/docsearch/node/docsearchPlugin.ts
@@ -1,31 +1,12 @@
-/*
-  Licensed to the Apache Software Foundation (ASF) under one
-  or more contributor license agreements.  See the NOTICE file
-  distributed with this work for additional information
-  regarding copyright ownership.  The ASF licenses this file
-  to you under the Apache License, Version 2.0 (the
-  "License"); you may not use this file except in compliance
-  with the License.  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing,
-  software distributed under the License is distributed on an
-  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-  KIND, either express or implied.  See the License for the
-  specific language governing permissions and limitations
-  under the License.
- */
-
-import type { Plugin } from '@vuepress/core';
-import { getDirname, path } from '@vuepress/utils';
+import type { Plugin } from 'vuepress/core';
+import { getDirname, path } from 'vuepress/utils';
 import type { DocsearchOptions } from '../shared/index.js';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
 const __dirname = getDirname(import.meta.url);
 
 /**
- * Options for @vuepress/plugin-docsearch-iotdb
+ * Options for @vuepress/plugin-docsearch
  */
 export interface DocsearchPluginOptions extends DocsearchOptions {
   /**
@@ -43,10 +24,10 @@ export const docsearchPlugin = ({
   injectStyles = true,
   indexBase,
   ...options
-}: DocsearchPluginOptions): Plugin => ({
+}: DocsearchPluginOptions = {}): Plugin => ({
   name: '@vuepress/plugin-docsearch-iotdb',
 
-  clientConfigFile: path.resolve(__dirname, '../client/config.js'),
+  clientConfigFile: path.resolve(__dirname, '../client/config.ts'),
 
   define: (app) => ({
     __DOCSEARCH_INJECT_STYLES__: injectStyles,
