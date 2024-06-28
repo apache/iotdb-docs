6d6fe96ccfccb0e21208fc70a70ad2692cdc7f9b
majialin <107627937+mal117@users.noreply.github.com>
2024-06-28 17:10:43 +0800

    Add "AINode Deployment" document (#260)

--- src/.vuepress/sidebar_timecho/V1.3.x/zh.ts
+++ src/.vuepress/sidebar_timecho/V1.3.x/zh.ts
@@ -73,6 +73,7 @@ export const zhSidebar = {
         { text: '单机版部署', link: 'Stand-Alone-Deployment_timecho' },       
         { text: '集群版部署', link: 'Cluster-Deployment_timecho' },
         { text: 'docker部署', link: 'Docker-Install' },
+        { text: 'AINode部署', link: 'AINode_Deployment' },
         { text: '工具部署（监控面板）', link: 'Monitoring-panel-deployment' },
       ],
     },
