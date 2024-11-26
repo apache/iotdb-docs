bbc90af866c53ffba61215ecf651ecd610d321cf
W1y1r <2730956796@qq.com>
2024-11-26 15:44:40 +0800

    Fix 404 issue and change 1.3.x to 1.3.3

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -18,7 +18,7 @@
  */
 
 import { sidebar } from 'vuepress-theme-hope';
-import { zhSidebar as V103xSidebar } from './V1.3.x/zh.js';
+import { zhSidebar as V103xSidebar } from './V1.3.3/zh.js';
 import { zhSidebar as V1030Sidebar } from './V1.3.0-2/zh.js';
 import { zhSidebar as V102xSidebar } from './V1.2.x/zh.js';
 import { zhSidebar as V101xSidebar } from './V1.1.x/zh.js';
