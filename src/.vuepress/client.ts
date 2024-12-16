e226bb3e9815d2281958da336660b2dea4a584a8
Mister-Hope <mister-hope@outlook.com>
2024-12-16 21:44:57 +0800

    fix: fix docsearch

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -3,13 +3,14 @@ import { computed } from 'vue';
 import { defineClientConfig, usePageData } from 'vuepress/client';
 import { getDocVersion } from './utils/index.js';
 
-defineClientConfig({
+export default defineClientConfig({
   setup() {
     const pageData = usePageData();
 
     const docSearchConfig = computed(() => ({
       appId: 'JLT9R2YGAE',
       apiKey: 'f1f30c0df04d74534e066d07786bce05',
+      indexName: 'iotdb-apache',
       maxResultsPerGroup: 10,
       disableUserPersonalization: true,
       searchParameters: {
