db2c7d867c852b5b20df3813a9caeb629ea75c3c
W1y1r <2730956796@qq.com>
2025-07-04 22:54:45 +0800

    FAQ synchronized to table model

--- src/.vuepress/sidebar/V2.0.x/en-Table.ts
+++ src/.vuepress/sidebar/V2.0.x/en-Table.ts
@@ -224,5 +224,16 @@ export const enSidebar = {
         { text: 'System Tables', link: 'System-Tables' },
       ],
     },
+    {
+      text: 'FAQ',
+      collapsible: true,
+      prefix: 'FAQ/',
+      children: [
+        {
+          text: 'Frequently Asked Questions',
+          link: 'Frequently-asked-questions',
+        },
+      ],
+    },
   ],
 };
