5e009533c318a422f1b7fe4f7b60aae7b1ffb477
CritasWang <critas@outlook.com>
2026-02-04 18:52:33 +0800

    add Captcha tips (#999)

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -49,8 +49,9 @@ const kapaConfig = {
   'data-deep-thinking-button-active-bg-color': '#F6F7F8',
   'data-deep-thinking-button-active-text-color': '#9E2878',
   'data-deep-thinking-button-active-hover-text-color': '#9E2878',
-  'data-modal-disclaimer':
-    'This is a custom LLM for Apache IoTDB with access to all [documentation](iotdb.apache.org/docs/), [GitHub Open Issues, PRs and READMEs](github.com/apache/iotdb).&#10;&#10;Companies deploy assistants like this ([built by kapa.ai](https://kapa.ai)) on docs via [website widget](https://docs.kapa.ai/integrations/website-widget) (Docker, Reddit), in [support forms](https://docs.kapa.ai/integrations/support-form-deflector) for ticket deflection (Monday.com, Mapbox), or as [Slack bots](https://docs.kapa.ai/integrations/slack-bot) with private sources.',
+  'data-modal-disclaimer': `This is a custom LLM for Apache IoTDB with access to all [documentation](iotdb.apache.org/docs/), [GitHub Open Issues, PRs and READMEs](github.com/apache/iotdb).&#10;&#10;
+    
+If you encounter "Error in verifying browser for feedback submission. Captcha token could not be obtained." please ensure that you can access Google services.`,
   'data-user-analytics-fingerprint-enabled': 'true',
   'data-consent-required': 'true',
   'data-consent-screen-disclaimer':
@@ -77,8 +78,9 @@ export default defineUserConfig({
           {
             ...kapaConfig,
             'data-language': 'zh',
-            'data-modal-disclaimer':
-              '这是一个针对 Apache IoTDB 的定制化大型语言模型，能够访问所有[文档](iotdb.apache.org/docs/)、[GitHub 公开问题、PR 和自述文件](github.com/apache/iotdb)。&#10;&#10;许多公司通过 [kapa.ai 构建](https://kapa.ai) 的助手部署在文档上，方式包括通过[网站小部件](https://docs.kapa.ai/integrations/website-widget)（Docker、Reddit）、在[支持表单](https://docs.kapa.ai/integrations/support-form-deflector)中进行工单偏转（Monday.com、Mapbox），或作为具有私有源的[Slack 机器人](https://docs.kapa.ai/integrations/slack-bot)。',
+            'data-modal-disclaimer': `这是一个针对 Apache IoTDB 的定制化大型语言模型，能够访问所有[文档](iotdb.apache.org/docs/)、[GitHub 公开问题、PR 和自述文件](github.com/apache/iotdb)。&#10;&#10;
+
+如果您遇到"Error in verifying browser for feedback submission. Captcha token could not be obtained." 请确保您能够顺畅访问 Google 服务。`,
             'data-consent-screen-disclaimer':
               "点击<I agree, let's chat>即表示您同意按照 kapa.ai 的[隐私政策](https://www.kapa.ai/content/privacy-policy)使用 AI 助手。本服务使用 reCAPTCHA，您需要同意 Google 的[隐私政策](https://policies.google.com/privacy)和[服务条款](https://policies.google.com/terms)。继续操作即表示您明确同意 kapa.ai 和 Google 的隐私政策。",
           },
