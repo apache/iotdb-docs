9b98d6960658967708a52b1649c570317846601f
Leto_b <bingqian.bai@timecho.com>
2026-03-23 17:29:55 +0800

    add odbc from 2082

--- src/.vuepress/sidebar_timecho/V2.0.x/en-Tree.ts
+++ src/.vuepress/sidebar_timecho/V2.0.x/en-Tree.ts
@@ -222,6 +222,7 @@ export const enSidebar = {
         { text: 'Node.js Native API', link: 'Programming-NodeJS-Native-API' },
         { text: 'Rust Native API', link: 'Programming-Rust-Native-API' },
         { text: 'JDBC', link: 'Programming-JDBC_timecho' },
+        { text: 'ODBC', link: 'Programming-ODBC_timecho' },
         { text: 'MQTT Protocol', link: 'Programming-MQTT' },
         { text: 'OPC UA Protocol', link: 'Programming-OPC-UA_timecho' },
         { text: 'OPC DA Protocol', link: 'Programming-OPC-DA_timecho' },
--- src/.vuepress/sidebar_timecho/V2.0.x/zh-Table.ts
+++ src/.vuepress/sidebar_timecho/V2.0.x/zh-Table.ts
@@ -193,6 +193,7 @@ export const zhSidebar = {
         { text: 'GO原生接口', link: 'Programming-Go-Native-API_timecho' },
         { text: 'C#原生接口', link: 'Programming-CSharp-Native-API_timecho' },
         { text: 'JDBC', link: 'Programming-JDBC_timecho' },
+        { text: 'ODBC', link: 'Programming-ODBC_timecho' },
         { text: 'MQTT协议', link: 'Programming-MQTT' },
         { text: 'RESTAPI V1 ', link: 'RestServiceV1' },
       ],
