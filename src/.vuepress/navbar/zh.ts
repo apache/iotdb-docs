4f12f6dc7296ca1092ac50d2df15df543d3a1a7e
leto-bbq <bingqian.bai@timecho.com>
2026-09-24 18:48:29 +0800

    Add community ecosystem pages (#1231)

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -49,6 +49,7 @@ export const zhNavbar = navbar([
       { text: '关于社区', link: '/zh/Community/About' },
       { text: '贡献指南', link: '/zh/Community/Development-Guide' },
       { text: '社区伙伴', link: '/zh/Community/Community-Partners' },
+      { text: '社区生态', link: '/zh/Community/Community-Ecosystem' },
       { text: '交流与反馈', link: '/zh/Community/Feedback' },
       { text: '活动与报告', link: 'zh/Community/Events-and-Reports' },
       { text: 'Committers', link: '/zh/Community/Committers' },
